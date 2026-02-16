5347678a5390f730f0597fe37bb0b1495696db01
Кугона <152641799+kugona@users.noreply.github.com>
2026-02-16 21:07:26 +0300

    Update xray-exporter ExecStart parameters
    Added log path and version to xray-exporter command.

--- exporter.bash
+++ exporter.bash
@@ -157,7 +157,7 @@ User=xray_exporter
 Group=xray_exporter
 Type=simple
 ExecStart=/usr/local/bin/xray-exporter \
-  --xray-endpoint=127.0.0.1:54321   # ← ИЗМЕНИТЕ НА РЕАЛЬНЫЙ АДРЕС API Xray (обычно 127.0.0.1:54321 или unix socket)
+  --xray-endpoint=127.0.0.1:54321 --log-path=/var/log/remnanode/access.log --version
 Restart=always
 RestartSec=10
 
